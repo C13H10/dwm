57c77dab9904cfd96d450283002adc3ecf3fbd74
soray <sora.yang@tutanota.com>
2022-02-28 21:03:55 -0600

    a

--- vnla-dwm/.dwm/autostart.sh
+++ vnla-dwm/.dwm/autostart.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-picom --config ~/.dwm/picom/picom.conf &
+picom &
 redshift -P -O 4500
-dunst &
-brightnessctl set 75
-nm-applet &
 ~/.fehbg
-~/.dwm/polybar/launch.sh &
-sxhkd -c ~/.dwm/sxhkd/sxhkdrc &
 ~/.dwm/bar/bar.sh &
 dwm
